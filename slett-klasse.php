b5d85eed1526749364c3e9a811ef9569647bad0e
Einar Helle <einarhelle@yahoo.com>
2025-10-27 14:05:48 +0100

    slett klasse

--- slett-klasse.php
+++ slett-klasse.php
@@ -9,39 +9,39 @@
 
 <h3>Slett klasse</h3>
 
-<form method="post" action="" id="slettPoststedSkjema" name="slettPoststedSkjema" onSubmit="return bekreft()">
-  Postnr <input type="text" id="postnr" name="postnr" required /> <br/>
-  <input type="submit" value="Slett poststed" name="slettPoststedKnapp" id="slettPoststedKnapp" /> 
+<form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onSubmit="return bekreft()">
+  Postnr <input type="text" id="klasse" name="klasse" required /> <br/>
+  <input type="submit" value="Slett klasse" name="slettklasseKnapp" id="slettklasseKnapp" /> 
 </form>
 
 <?php
-  if (isset($_POST ["slettPoststedKnapp"]))
+  if (isset($_POST ["slettklasseKnapp"]))
     {	
-      $postnr=$_POST ["postnr"];
+      $klasse=$_POST ["klasse"];
 	  
-	  if (!$postnr)
+	  if (!$klasse)
         {
-          print ("Postnr m&aring; fylles ut");
+          print ("Klasse m&aring; fylles ut");
         }
       else
         {
           include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
-          $sqlSetning="SELECT * FROM poststed WHERE postnr='$postnr';";
+          $sqlSetning="SELECT * FROM klasse WHERE klasse='$klasse';";
           $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
           $antallRader=mysqli_num_rows($sqlResultat); 
 
           if ($antallRader==0)  /* poststedet er ikke registrert */
             {
-              print ("Poststedet finnes ikke");
+              print ("Klassen finnes ikke");
             }
           else
             {	  
-              $sqlSetning="DELETE FROM poststed WHERE postnr='$postnr';";
+              $sqlSetning="DELETE FROM klasse WHERE klasse='$klasse';";
               mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
                 /* SQL-setning sendt til database-serveren */
 		
-              print ("F&oslash;lgende poststed er n&aring; slettet: $postnr  <br />");
+              print ("F&oslash;lgende klasse er n&aring; slettet: $klasse  <br />");
             }
         }
     }
