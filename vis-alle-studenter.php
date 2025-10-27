1ddf482e142196927a3edb5ed781c6cfeffa6cf3
Einar Helle <einarhelle@yahoo.com>
2025-10-27 13:53:56 +0100

    mer ting

--- vis-alle-studenter.php
+++ vis-alle-studenter.php
@@ -1,5 +1,30 @@
-<?php
+<?php  /* vis-alle-poststeder */
+/*
+/*  Programmet skriver ut alle registrerte poststeder
+*/
+  include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
-print ("<h3>Vis alle studenter</h3>"); 
+  $sqlSetning="SELECT * FROM studenter;";
+  
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+    /* SQL-setning sendt til database-serveren */
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
+  print ("<h3>Registrerte studenter</h3>");
+  print ("<table border=1>");  
+  print ("<tr><th align=left>brukernavn</th> <th align=left>fornavn</th> <th align=left>etternavn</th> <th align=left>klassekode</th> </tr>"); 
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra sp�rringsresultatet */
+      $brukernavn=$rad["brukernavn"];        
+      $fornavn=$rad["fornavn"];    
+      $etternavn=$rad["etternavn"]; 
+      $klassekode=$rad["klassekode"];       
+
+
+      print ("<tr> <td> $brkernavn </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>");
+    }
+  print ("</table>"); 
 ?>
