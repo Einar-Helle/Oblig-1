46ecc8371a431dc8b511dc0fcfa150f64fd78a07
Einar-Helle <einarhelle@yahoo.com>
2025-11-03 12:17:12 +0100

    Update slett-student.php

--- slett-student.php
+++ slett-student.php
@@ -10,7 +10,7 @@
 <h3>Slett student</h3>
 
 <form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema" onSubmit="return bekreft()">
-  Postnr <input type="text" id="postnr" name="postnr" required /> <br/>
+  Student <input type="text" id="student" name="student" required /> <br/>
   <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" /> 
 </form>
 
